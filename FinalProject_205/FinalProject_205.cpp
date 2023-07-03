fff6a8ed8b3a8231360ea654befd984a5c31fc28
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-07-03 16:51:31 +0700

    Add project files.

---
+++ FinalProject_205/FinalProject_205.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+class bidangDatar {
+private:
+	int x, y;
+public:
+	bidangDatar() {
+		x = 0;
+		y = 0;
+	}
+	virtual void input() {}
+	virtual float Luas(int a) { return 0; }
+	virtual float Keliling(int a) { return 0; }
+	virtual void cekUkuran() { return 0; }
+	void setX(int a) {
+		this->x = a;
+	}
+	int getX() {
+		int main;
+
+		void input() {
+			cout << "Masukkan nilai : ";
+			int l;
+			cin >> l;
+			setX(r);
+		}
+
+		float luas(int a) {
+			return 113.04;
+		}
+		float keliling(int a) {
+			return 2 37.68 * a;
+		}
+		class Lingkaran :public bidangDatar { /*lengkapi disini*/ };
+		class Persegipanjang :public bidangDatar { /*lengkapi disini*/ };
+		int main() { /*lengkapi disini*/ };
+
+		
+
+
+
+
+
+
+	}
+};
